c916b44ad4f1a8db7e9555ee2a799ac0cc459d47
Dmitriy <44997536+DmitriyBartosh@users.noreply.github.com>
2022-02-23 03:53:14 +0700

    add

--- src/components/about/presentation.js
+++ src/components/about/presentation.js
@@ -1,38 +1,110 @@
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
-import { MdOutlineExpandMore } from "react-icons/md";
-import { container, preview, video, info } from "./presentation.module.scss";
+import { BsChevronDoubleDown } from "react-icons/bs";
+import { btn } from "../../style/other.module.scss";
+import {
+  presentation,
+  container,
+  preview,
+  video,
+  photo,
+  info,
+  scrolldown,
+  left,
+  right,
+  backimgone,
+  backimgtwo,
+} from "./presentation.module.scss";
 
 function Presentation() {
   return (
-    <div className={container}>
-      <MdOutlineExpandMore />
-      <h2>О нас</h2>
-      <p>
-        Студия основана в 2010 году, основой нашей деятельности является:
-        массаж, фитобочка, релакс программы (спа), оздоровительные сеансы
-        соляной комнаты. Все для того, чтобы человек жил в гармонии здорового
-        тела, в здоровом физическом и душевном состоянии. Мы профессионально
-        практикуем массажные техники различных видов.
-      </p>
-      <div className={preview}>
-        <div className={video}>
-          <video autoPlay muted loop playsInline>
-            <source src="/video/1.mp4" type="video/mp4" />
-          </video>
+    <div className={presentation}>
+      <StaticImage
+        src="../../images/about/back1.png"
+        alt="массаж"
+        className={backimgone}
+      />
+      <div className={container}>
+        <BsChevronDoubleDown className={scrolldown} />
+        <h2>О нас</h2>
+        <p style={{ textAlign: "center" }}>
+          Студия основана в 2010 году, основой нашей деятельности является:
+          массаж, фитобочка, релакс программы (спа), оздоровительные сеансы
+          соляной комнаты. Все для того, чтобы человек жил в гармонии здорового
+          тела, в здоровом физическом и душевном состоянии. Мы профессионально
+          практикуем массажные техники различных видов.
+        </p>
+        <div className={preview}>
+          <div className={video}>
+            <video autoPlay muted loop playsInline>
+              <source src="/video/1.mp4" type="video/mp4" />
+            </video>
+          </div>
+          <div className={video}>
+            <video autoPlay muted loop playsInline>
+              <source src="/video/2.mp4" type="video/mp4" />
+            </video>
+          </div>
+          <div className={video}>
+            <video autoPlay muted loop playsInline>
+              <source src="/video/3.mp4" type="video/mp4" />
+            </video>
+          </div>
+
+          <StaticImage
+            src="../../images/about/back2.png"
+            alt="массаж png"
+            className={backimgtwo}
+          />
         </div>
-        <div className={video}>
-          <video autoPlay muted loop playsInline>
-            <source src="/video/2.mp4" type="video/mp4" />
-          </video>
+        <div className={info}>
+          <div className={left}>
+            <div className={photo}>
+              <StaticImage
+                src="../../images/about/1.jpeg"
+                alt="ложка меда массажная студия"
+              />
+            </div>
+          </div>
+          <div className={right}>
+            <h3>
+              Наша специализация:
+              <br />
+              оздоровление, релакс-программы
+            </h3>
+            <p>
+              В нашей студии городского формата есть чудесные кедровые фитобочки
+              с целебным влажным паром с использованием арома масел
+              <br />
+              <br />
+              Вкусный травяной чай с традиционной ложкой меда Квалифицированные
+              специалисты
+            </p>
+            <button className={btn}>Выбрать услугу</button>
+          </div>
         </div>
-        <div className={video}>
-          <video autoPlay muted loop playsInline>
-            <source src="/video/3.mp4" type="video/mp4" />
-          </video>
+        <div className={info}>
+          <div className={left}>
+            <p>
+              Есть возможность воспользоваться нашими программами одновременно
+              вдвоем, с вашими близкими людьми, а также приобрести сертификаты
+              на любую услугу или номинал
+            </p>
+            <Link to="certificate" className={btn}>
+              Подарочные сертификаты
+            </Link>
+          </div>
+          <div className={right}>
+            <div className={photo}>
+              <StaticImage
+                src="../../images/about/2.JPG"
+                alt="ложка меда массажная студия"
+              />
+            </div>
+          </div>
         </div>
       </div>
-      <div className={info}></div>
-      <div className={info}></div>
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "gatsby";
+import { useWindowScroll } from "react-use";
 import { IoLogoInstagram, IoLogoVk } from "react-icons/io5";
-import { container, link } from "./header.module.scss";
+import { IoReorderThree } from "react-icons/io5";
+import {
+  container,
+  link,
+  hideLink,
+  burger,
+  mobile,
+  desktop,
+  active,
+} from "./header.module.scss";
 
 function Header() {
+  const { y } = useWindowScroll();
+  const [positionY, setPositionY] = useState(0);
+  const [showHeader, setShowHeader] = useState(true);
+
+  useEffect(() => {
+    if (y < positionY) {
+      setShowHeader(true);
+      setPositionY(y);
+    } else {
+      setShowHeader(false);
+      setPositionY(y);
+    }
+  }, [y]);
+
   return (
     <div className={container}>
-      <a
-        href="https://www.instagram.com/studio_massage_/"
-        target="_blank"
-        rel="noreferrer"
-        className={link}
-      >
-        <IoLogoInstagram />
-      </a>
-      <a
-        href="https://vk.com/fitochka24"
-        target="_blank"
-        rel="noreferrer"
-        className={link}
-      >
-        <IoLogoVk />
-      </a>
+      <div className={mobile}>
+        <Link
+          to="/"
+          className={link}
+          style={{ transform: !showHeader && "translateY(-60px)" }}
+          activeClassName={active}
+        >
+          О нас
+        </Link>
+        <Link
+          to="/massage"
+          className={link}
+          style={{ transform: !showHeader && "translateY(-60px)" }}
+          activeClassName={active}
+        >
+          Массаж
+        </Link>
+        <Link
+          to="/relaxspa"
+          className={link}
+          style={{ transform: !showHeader && "translateY(-60px)" }}
+          activeClassName={active}
+        >
+          Relax & SPA
+        </Link>
+        <Link to="/contact" className={burger} activeClassName={active}>
+          <IoReorderThree />
+        </Link>
+      </div>
+      <div className={desktop}>
+        <a
+          href="https://www.instagram.com/studio_massage_/"
+          target="_blank"
+          rel="noreferrer"
+          className={link}
+        >
+          <IoLogoInstagram />
+        </a>
+        <a
+          href="https://vk.com/fitochka24"
+          target="_blank"
+          rel="noreferrer"
+          className={link}
+        >
+          <IoLogoVk />
+        </a>
+      </div>
     </div>
   );
 }
